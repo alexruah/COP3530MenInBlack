f72664bae0644812e10a705f3e2aeaa7b43726a6
ahp64 <101662343+ahp64@users.noreply.github.com>
2023-12-04 01:22:52 -0500

    hello

---
+++ my_kd_tree.h
@@ -0,0 +1,21 @@
+//
+// Created by Aidan Parsons on 12/3/23.
+//
+
+#ifndef COP3530MENINBLACK_MY_KD_TREE_H
+#define COP3530MENINBLACK_MY_KD_TREE_H
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <pair>
+
+using namespace std;
+class my-kd-tree{
+    private:
+        
+
+};
+
+
+#endif //COP3530MENINBLACK_MY_KD_TREE_H
