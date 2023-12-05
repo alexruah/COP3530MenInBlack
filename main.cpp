dc9f07952136593092134eec398f2e4afce5a6be
ahp64 <101662343+ahp64@users.noreply.github.com>
2023-12-05 01:48:30 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 
 #include "graph.h"
 using namespace sf;
+// Setting up input
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* data) {
     size_t totalSize = size * nmemb;
     data->append(static_cast<char*>(contents), totalSize);
@@ -19,6 +20,8 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* dat
 }
 
 int main(){
+
+    // Use curl to take the input from the files
     CURL* curl;
     CURLcode res;
 
@@ -64,6 +67,7 @@ int main(){
         std::vector<std::vector<std::string>> bigfoot_coordinates;
         int buffer = -1;
 
+        // Read all the alien coordinates, put in a vector
         while (std::getline(csvStream, line)) {
 
             if (buffer == -1) {
@@ -96,7 +100,7 @@ int main(){
 
         buffer = -1;
 
-
+        // Read all the bigfoot coordinates, put in a vector
         while (std::getline(bigfootData, line)) {
 
             if (buffer == -1) {
@@ -135,14 +139,13 @@ int main(){
 
             bigfoot_coordinates.push_back(row);
         }
-
-        cout << "test" << endl;
-        //cout << bigfoot_coordinates[1][0] << " " << bigfoot_coordinates[1][1] << endl;
+        /*NOTE: LEAVE THE TOP TWO OF THIS IN FOR KD LIST, LEAVE THE BOTTOM TWO IN FOR GRAPH (GRAPH TAKES A LONG TIME)*/
         MyKDTree* alien_kd = new MyKDTree(alien_coordinates);
         MyKDTree* bigfoot_kd = new MyKDTree(bigfoot_coordinates);
       //  Graph* alien_graph = new Graph(alien_coordinates);
      //   Graph* bigfoot_graph = new Graph(bigfoot_coordinates);
 
+     // Setting up SFML window with MIB background
         srand(time(NULL));
         ifstream config("boards/config.cfg");
         RenderWindow window(VideoMode(1000, 667), "Men In Black");
@@ -151,51 +154,72 @@ int main(){
         sf::Sprite s(t);
         window.setVerticalSyncEnabled(true);
         window.clear(Color::Black);
+
+        // Setting up Input text objects and output text object with appropriate fonts
         Font input_font;
         if (!input_font.loadFromFile("/Users/aidanparsons/CLionProjects/COP3530MenInBlack/DIGITALDREAMFAT.ttf"))
             return EXIT_FAILURE;
         Font output_font;
         if (!output_font.loadFromFile("/Users/aidanparsons/CLionProjects/COP3530MenInBlack/arial.ttf"))
             return EXIT_FAILURE;
+
         sf::String playerInput1;
         sf::String playerInput2;
         sf::String output;
+
         sf::Text playerText1("Latitude", input_font, 24);
         sf::Text playerText2("Longitude", input_font, 24);
         sf::Text output_text("", output_font, 35);
+
         playerText1.setPosition(357, 281);
         playerText2.setPosition(357, 340);
         output_text.setPosition(250, 500);
+
         playerText1.setColor(sf::Color::Green);
         playerText2.setColor(sf::Color::Green);
         output_text.setColor(sf::Color::White);
+
+        // When true, the top input box is the one selected for typing. When false, the bottom input box in the one selected.
         bool top_or_bottom = true;
+
+        // Number of sightings near most recent coords
         int num_alien_sightings = 0;
         int num_bigfoot_sightings = 0;
+
         while (window.isOpen()) {
             sf::Event event;
 
             while (window.pollEvent(event)) {
 
                 switch (event.type) {
+
+                    // If user tries to close the window, close the window :)
                     case sf::Event::Closed:
                         window.close();
                         cout << "CLOSED" << endl;
                         break;
+
+                    // If the mouse button is pressed, figure out if the user was trying to select the top or bottom text inputs.
                     case Event::MouseButtonPressed:
-                        //window.close();
                         if (Mouse::getPosition(window).x > 357 && Mouse::getPosition(window).x < 457){
                             if (Mouse::getPosition(window).y < 305 && Mouse::getPosition(window).y > 281) {
+
                                 top_or_bottom = true;
+
                             } else if (Mouse::getPosition(window).y < 375 && Mouse::getPosition(window).y > 347){
+
                                 top_or_bottom = false;
+
                             }
                         }
-
-                      //  cout << "CLOSED" << endl;
-
                         break;
+
+                    // If text was entered, check what it is. If it's numbers or the - or . symbols, enter it into the currently
+                    // selected input box. If it's a backspace, shorten the string by 1. If it's an enter, compute and display the
+                    // nearby sightings.
                     case sf::Event::TextEntered:
+
+                        // Number, -, or .
                         if((event.text.unicode > 47 && event.text.unicode < 58) || (event.text.unicode > 44 && event.text.unicode < 47)) {
                             if (top_or_bottom) {
                                 playerInput1 += event.text.unicode;
@@ -204,25 +228,36 @@ int main(){
                                 playerInput2 += event.text.unicode;
                                 playerText2.setString(playerInput2);
                             }
+                        // Enter
                         } else if (event.text.unicode == 10){
                             if (playerInput1 != "" && playerInput2 != "") {
+
+                                /* NOTE: COMMENT OUT THE TOP TWO FOR KD TREE IMPLEMENTATION, COMMENT OUT THE BOTTOM TWO FOR GRAPH*/
                                 num_alien_sightings = alien_kd->RangeSearch(playerInput1, playerInput2);
                                 num_bigfoot_sightings = bigfoot_kd->RangeSearch(playerInput1, playerInput2);
                                // num_alien_sightings = alien_graph->getAbductionCount(playerInput1, playerInput2);
                                // num_bigfoot_sightings = bigfoot_graph->getAbductionCount(playerInput1, playerInput2);
+
+
                                 output = "THERE ARE " + std::to_string(num_alien_sightings) +
                                          " ALIEN SIGHTINGS\n AND " + std::to_string(num_bigfoot_sightings) + " BIGFOOT SIGHTINGS\n IN YOUR AREA";
                             } else {
                                 output = "";
                             }
                             output_text.setString(output);
+
+                            // Reset the input values
                             playerInput1 = "";
                             playerText1.setString("LATITUDE");
                             playerInput2 = "";
                             playerText2.setString("LONGITUDE");
+
+                        // Backspace
                         } else if (event.text.unicode == 8){
                             if (top_or_bottom && !playerInput1.isEmpty()) {
                                 playerInput1.erase(playerInput1.getSize() - 1);
+
+                                // Avoid error if empty, just switch back to LATITUDE
                                 if (playerInput1.isEmpty()){
                                     playerText1.setString("LATITUDE");
                                 } else {
@@ -230,6 +265,8 @@ int main(){
                                 }
                             } else if (!playerInput2.isEmpty()) {
                                 playerInput2.erase(playerInput2.getSize() - 1);
+
+                                // Avoid error if empty, just switch back to LONGITUDE
                                 if (playerInput2.isEmpty()){
                                     playerText2.setString("LONGITUDE");
                                 } else {
@@ -239,6 +276,8 @@ int main(){
                         }
                 }
             }
+
+            // Render the new values for this cycle.
             window.draw(s);
             window.draw(playerText1);
             window.draw(playerText2);
@@ -246,7 +285,6 @@ int main(){
             window.display();
             window.clear(Color::Black);
         }
-    //    cout << alien_kd->RangeSearch("29.6520", "-82.3250") << endl;
     }
 
     curl_global_cleanup();
